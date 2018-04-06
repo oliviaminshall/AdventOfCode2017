6241b5e984948da840cdf7b84c1d325c49d31958
Olivia Minshall <olivia.minshall@capgemini.com>
2018-04-06 10:09:05 +0100

    updated logic

--- Day4Part2.py
+++ Day4Part2.py
@@ -1,20 +1,17 @@
 # for each passphrase, for each word, check against each other word
 
 f = open("Day4.txt","r")
-
 counter = 0
 
 for line in f:
+    d = []
     line = line.strip("\n")
     words = line.split(" ")
     for word in words:
         b = sorted(word)
         c = ''.join(b)
-        words.remove(word)
-        words.append(c)
-    un = list(set(words))
-    print(un)
-    print(words)
+        d.append(c)
+    un = list(set(d))
     if(len(un) == len(words)):
         counter = counter + 1
 print(counter)
